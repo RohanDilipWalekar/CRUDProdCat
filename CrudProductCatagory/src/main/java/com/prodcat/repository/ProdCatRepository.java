d9efbfc63fe85bf94d3640ddbc0fe9741b65157c
RohanDilipWalekar <95201360+RohanDilipWalekar@users.noreply.github.com>
2025-01-30 17:20:00 +0530

    Delete CrudProductCatagory directory

--- CrudProductCatagory/src/main/java/com/prodcat/repository/ProdCatRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prodcat.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prodcat.entity.Category;
-
-public interface ProdCatRepository extends JpaRepository<Category, Long>{
-
-}
